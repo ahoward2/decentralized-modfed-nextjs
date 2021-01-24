5283d2fb88582da4d7889b3654ca1cc56dda3a6e
Austin Howard <ahoward2@unca.edu>
2021-01-24 17:56:43 -0500

    🛠  abstract out data source and add comments

--- tools/components/Calculator.jsx
+++ tools/components/Calculator.jsx
@@ -1,53 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
-import lodash, { clone, last } from "lodash";
+import lodash from "lodash";
 import Stock from "./Stock";
-
-const stocks = [
-  {
-    name: "voo",
-    value: 10765.35,
-    dividend: 1.5,
-    reinvestDividend: false,
-  },
-  {
-    name: "vti",
-    value: 230.02,
-    dividend: 1.38,
-    reinvestDividend: false,
-  },
-  {
-    name: "nvda",
-    value: 140.97,
-    dividend: 0.12,
-    reinvestDividend: false,
-  },
-  {
-    name: "aapl",
-    value: 601.79,
-    dividend: 0.61,
-    reinvestDividend: false,
-  },
-  {
-    name: "ba",
-    value: 9.73,
-    dividend: 1.98,
-    reinvestDividend: false,
-  },
-  {
-    name: "wmt",
-    value: 49.31,
-    dividend: 1.49,
-    reinvestDividend: false,
-  },
-  {
-    name: "msft",
-    value: 140.01,
-    dividend: 0.93,
-    reinvestDividend: false,
-  },
-];
-
-const updateStockValue = (stockArray, valueToUpdate) => {};
+import stocks from "./../utils/stocks";
 
 // input: array of stocks
 // compute: quarterly dividend accounting
@@ -92,6 +46,7 @@ const computeAnnualTotal = (stocksArray) => {
   return total;
 };
 
+// Calculator component holds dividend calculator
 const Calculator = (props) => {
   // deep clone of initial state of stock array using lodash's _.cloneDeep function
   const [clonedStocksArray, setClonedStocksArray] = useState(
@@ -105,9 +60,7 @@ const Calculator = (props) => {
   const [totalQuarterly, setTotalQuarterly] = useState(
     computeQuarterlyTotal(clonedStocksArray)
   );
-
-  const [lastStock, setLastStock] = useState("Last Stock");
-
+  // callback for toggling reinvesting dividends option for local stocks array
   const updateLastStock = useCallback(
     (n) => {
       setLastStock(() => {
@@ -125,7 +78,7 @@ const Calculator = (props) => {
     [setClonedStocksArray]
   );
 
-  // goal is to fire re-compute functions for totals when the local array copy changes
+  // fire re-compute functions for annual return total when the local array copy changes
   useEffect(() => {
     console.log("running useEffect()");
     setTotalAnnual(computeAnnualTotal(clonedStocksArray));
---
+++ tools/utils/stocks.js
@@ -0,0 +1,46 @@
+const stocks = [
+  {
+    name: "voo",
+    value: 765.35,
+    dividend: 1.5,
+    reinvestDividend: false,
+  },
+  {
+    name: "vti",
+    value: 230.02,
+    dividend: 1.38,
+    reinvestDividend: false,
+  },
+  {
+    name: "nvda",
+    value: 140.97,
+    dividend: 0.12,
+    reinvestDividend: false,
+  },
+  {
+    name: "aapl",
+    value: 601.79,
+    dividend: 0.61,
+    reinvestDividend: false,
+  },
+  {
+    name: "ba",
+    value: 9.73,
+    dividend: 1.98,
+    reinvestDividend: false,
+  },
+  {
+    name: "wmt",
+    value: 49.31,
+    dividend: 1.49,
+    reinvestDividend: false,
+  },
+  {
+    name: "msft",
+    value: 140.01,
+    dividend: 0.93,
+    reinvestDividend: false,
+  },
+];
+
+export default stocks;
